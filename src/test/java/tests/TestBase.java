f8ceb60ccde67301a235a8c26dbdf0403b37285a
Alexander Shashkin <caezar1993@gmail.com>
2022-11-11 19:00:58 +0400

    Remote launch set up

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,17 +1,42 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static java.lang.String.format;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
 
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
-        Configuration.baseUrl = "https://ya.ru";
-        Configuration.browserVersion = "99";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+        String browser = System.getProperty("browser", "chrome");
+        String baseUrl = System.getProperty("baseUrl", "https://ya.ru");
+        String browserVersion = System.getProperty("browserVersion", "99");
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browserSize = browserSize;
+        Configuration.baseUrl = baseUrl;
+        Configuration.browser = browser;
+        Configuration.browserVersion = browserVersion;
+
+        String remoteBrowserUser = credentials.remoteBrowserUser();
+        String remoteBrowserPassword = credentials.remoteBrowserPassword();
+        String browserURL = System.getProperty("url", credentials.remoteBrowserURL());
+        Configuration.remote = format("https://%s:%s@%s", remoteBrowserUser, remoteBrowserPassword, browserURL);
     }
 
 
